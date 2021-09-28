5105533321b969c8e8bc83a1e9174fa5c1cee4be
Nandini Rajaram <nandinirajaram@gmail.com>
2021-09-28 17:03:57 -0500

    PROJECT

---
+++ AzureDevops/src/test/java/Azuredevops/AzureDevops/Test1.java
@@ -0,0 +1,12 @@
+package Azuredevops.AzureDevops;
+
+import org.testng.annotations.Test;
+
+public class Test1 {
+  @Test
+  public void f() {
+	  System.out.println("**************************");
+	  System.out.println("Hello World Test");
+	  System.out.println("**************************");
+  }
+}
